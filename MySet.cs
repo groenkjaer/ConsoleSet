7a2c0b6a126f2632e765428bbce26fd5e4d6add4
Fillip Grønkjær Jensen <fill0246@student.eadania.dk>
2020-03-01 14:43:43 +0100

    Add project files.

---
+++ MySet.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ConsoleSet
+{
+    class MySet<T> : ISet<T>
+    {
+        private List<T> list = new List<T>();
+
+        public int Count
+        {
+            get { return list.Count; }
+        }
+
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+
+        public void Sort()
+        {
+            list.Sort();
+        }
+
+        public void AddRange(IEnumerable<T> collection)
+        {
+            list.AddRange(collection);
+        }
+
+        public void Remove(T item)
+        {
+            list.Remove(item);
+        }
+
+        public ISet<T> Intersect(ISet<T> S)
+        {
+            MySet<T> temp = new MySet<T>();
+
+            foreach (var item in list)
+            {
+                foreach (var secondItem in list)
+                {
+                    if (item.Equals(secondItem))
+                    {
+                        temp.Add(item);
+                    }
+                }
+            }
+            return temp as MySet<T>;
+        }
+
+        public ISet<T> Difference(ISet<T> S)
+        {
+            list.AddRange(S);
+            list.Sort();
+            list = list.Distinct().ToList();
+            return list as MySet<T>;
+        }
+
+        public ISet<T> Union(ISet<T> S)
+        {
+            AddRange(S);
+            Sort();
+            list = list.Distinct().ToList();
+            return list as MySet<T>;
+        }
+
+        void ISet<T>.Remove(T elem)
+        {
+            Remove(elem);
+        }
+
+        public bool Contains(T elem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleSet
+{
+    class Program
+    {
+        private static Random rng = new Random();
+        static void Main(string[] args)
+        {
+            Test1();
+        }
+
+        private static void Test1()
+        {
+            MySet<int> ms1 = new MySet<int>();
+            MySet<int> ms2 = new MySet<int>();
+
+            for (int i = 0; i < 50; i++)
+            {
+                ms1.Add(rng.Next(20, 51));
+                ms2.Add(rng.Next(20, 51));
+            }
+
+            PrintList(ms1);
+            PrintList(ms2);
+
+            MySet<int> wpjdaw = new MySet<int>();
+            wpjdaw.AddRange(ms1.Intersect(ms2));
+            PrintList(wpjdaw);
+            
+        }
+
+        private static void PrintList(MySet<int> nogetandet)
+        {
+            nogetandet.Sort(); //BROKE
+            foreach (var item in nogetandet)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
